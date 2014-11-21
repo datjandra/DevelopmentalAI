27522db643504d819d0c3769a75e11914a0e34b3
datjandra <datjandra@lbl.gov>
2014-11-21 10:43:18 -0800

    Add maze environment

--- src/homework/radical/interactionism/Main.groovy
+++ src/homework/radical/interactionism/Main.groovy
@@ -1,12 +1,29 @@
 package homework.radical.interactionism
 
+import environment.Environment
+import environment.EnvironmentMaze
 import existence.Existence
 import existence.Existence050
 
 class Main {
 
+	static class Existence050WithMaze extends Existence050 {
+		
+		private Environment localEnvironment
+		
+		@Override
+		protected void initExistence() {
+			localEnvironment = new EnvironmentMaze(this)
+		}
+		
+		@Override
+		protected Environment getEnvironment() {
+			return localEnvironment
+		}
+	}
+	
 	static main(args) {
-		Existence existence = new Existence050()
+		Existence existence = new Existence050WithMaze()
 		(1..20).each {
 			println "${it} - ${existence.step()}"
 		}
